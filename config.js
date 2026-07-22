47c3aa6c5c55cb67bb95f394df5659d8c2e8eb69
Pendukung-OkeGas <muhammadfaizun.23178@mhs.unesa.ac.id>
2026-07-22 19:38:12 +0700

    api kyy

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // Menggunakan environment variable dari Vercel
 // Di Vercel dashboard, set environment variable: GEMINI_API_KEY
 
-const GEMINI_API_KEY = '${GEMINI_API_KEY}';
+const GEMINI_API_KEY = `${GEMINI_API_KEY}`;
 
 function getApiKey() {
   return GEMINI_API_KEY || '';
